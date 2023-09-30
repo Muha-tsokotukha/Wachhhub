86b4826f134631374da7dcdb92fc776562c3b08a
Mukhit Home <vinniedrew7@gmail.com>
2023-09-30 22:41:45 +0600

    feat(components): custom if directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WatchListComponent } from 'src/components/watch-list/watch-list.component';
 import { RequestFormComponent } from 'src/components/request-form/request-form.component';
+import { CheckNameDirective } from 'src/components/watch-list/name-checker.component';
 
 @NgModule({
-  declarations: [AppComponent, WatchListComponent, RequestFormComponent],
+  declarations: [
+    AppComponent,
+    WatchListComponent,
+    RequestFormComponent,
+    CheckNameDirective,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
