23e626803fde31bab1ab2cd91895dbeceaef8bde
Diqosh <tdimasg@gmail.com>
2023-10-09 13:09:21 +0600

    feat(pwa): cache first app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { IUserRequest } from 'src/store';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +6,4 @@ import { IUserRequest } from 'src/store';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wachhhub';
-
-  requests: IUserRequest[] = [{ duration: 1, name: 'asd', category: 'horror' }];
-
-  addNewRequest(request: IUserRequest) {
-    this.requests.push(request);
-  }
 }
