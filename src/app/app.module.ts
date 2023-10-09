901d99d81a64e0bbf81935cfdc8fbe42be72b7b4
Diqosh <tdimasg@gmail.com>
2023-10-09 11:19:54 +0600

    feat(layout): added antd layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { AppComponent } from './app.component';
 import { WatchListComponent } from 'src/components/watch-list/watch-list.component';
 import { RequestFormComponent } from 'src/components/request-form/request-form.component';
 import { CheckNameDirective } from 'src/components/watch-list/name-checker.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+import {NzMenuModule} from "ng-zorro-antd/menu";
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -14,9 +26,13 @@ import { CheckNameDirective } from 'src/components/watch-list/name-checker.compo
     WatchListComponent,
     RequestFormComponent,
     CheckNameDirective,
+    HeaderComponent,
+    SidebarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, BrowserAnimationsModule, NzLayoutModule, NzMenuModule],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
